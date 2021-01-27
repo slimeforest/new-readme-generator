cf00af8f756d431d128813dc679235843ed3342b
jack battle <jackonno78@hotmail.com>
2021-01-26 20:34:50 -0600

    added files to .gitignore and fixed the bug, working application now

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# new-readme-generator
+# Easy README.md generator
+## Description of project
+A simple node.js app to quickly and consistently generate readme files for a project
+## Installation instructions
+download this index.js file and inquirer, run the index in node, answer the questions in the command line.
+## GitHub repository link
+https://github.com/slimeforest/new-readme-generator
+## eMail address
+jackonno78@hotmail.com
--- index.js
+++ index.js
@@ -1,48 +1,47 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-
-function init(){
+const inquirer = require('inquirer');
+const fs = require('fs');
+// name location, bio, linkedIn, GitHub
+function init() {
     inquirer
         .prompt([{
-            type: "input",
-            message: "Enter the name of the project.",
-            name: "projectName"
-        },
-        {
-            type: "input",
-            message: "Enter a description of the project",
-            name: "projectDescription"
-        },
-        {
-            type: "input",
-            message: "Provide installation instructions",
-            name: "projectInstructions"
-        },
-        {
-            type: "input",
-            message: "Enter your github repository link.",
-            name: "githubRepo"
-        },
-        {
-            type: "input",
-            message: "Enter your email address",
-            name: "projectEmail"
-        }        
-    ])
-    .then(response => {
-        let readme = `
-        # ${response.projectName}
-        ## Description of project
-        ${response.projectDescription}
-        ## Installation instructions
-        ${response.projectInstructions}
-        ## GitHub repository link
-        ${repsonse.githubRepo}
-        ## eMail address
-        ${response.projectEmail}
-        `
-            fs.writeFile("README.md", readme, (err) =>
-                err ? console.error(err) : console.log("Success!"))
-    })
+                type: 'input',
+                message: "Enter the name of the project.",
+                name: "projectName"
+            },
+            {
+                type: 'input',
+                message: "Enter a description of the project",
+                name: "projectDescription"
+            },
+            {
+                type: 'input',
+                message: "Provide installation instructions",
+                name: "projectInstall"
+            },
+            {
+                type: 'input',
+                message: "Enter your github repository link.",
+                name: "githubRepo"
+            },
+            {
+                type: 'input',
+                message: "Enter your email address",
+                name: "projectEmail"
+            }
+        ])
+        .then(response => {
+            let html = `# ${response.projectName}
+## Description of project
+${response.projectDescription}
+## Installation instructions
+${response.projectInstall}
+## GitHub repository link
+${response.githubRepo}
+## eMail address
+${response.projectEmail}
+`
+            fs.writeFile('README.md', html, (err) =>
+                err ? console.error(err) : console.log('Success!'))
+        })
 }
 init();
